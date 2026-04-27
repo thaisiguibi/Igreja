5d1b8c728c39cdc318ab4304c38ee3d5a9f406c4
Thaisiguibi <carnierithais@yahoo.com>
2026-04-27 18:13:30 -0300

    fix schema db correto

--- core/db.py
+++ core/db.py
@@ -4,9 +4,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 DB_PATH = os.path.join(BASE_DIR, "igreja.db")
 
-
-DB_PATH = "igreja.db"
-
 def get_connection():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
@@ -17,22 +14,26 @@ def init_db():
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute ("""
-    CREATE TABLE IF NOT EXISTS posts (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        content TEXT
-        active INTEGER DEFAULT 1
-        )
-        """)
+    cursor.execute("DROP TABLE IF EXISTS posts")
+
+    cursor.execute("""
+    CREATE TABLE posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT,
+    user_id INTEGER,
+    active INTEGER DEFAULT 1
+    )
+                   """)
 
     cursor.execute ("""
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
         active INTEGER DEFAULT 1
         )
-        """)
+                    """)
 
     conn.commit()
     conn.close()
