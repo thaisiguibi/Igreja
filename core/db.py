9015c29c378b3095e72762d63427fbc7393f83af
thaisiguibi <carnierithais@gmail.com>
2026-08-10 22:07:41 -0300

    Preserva dados na inicialização do banco

--- core/db.py
+++ core/db.py
@@ -4,46 +4,44 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 DB_PATH = os.path.join(BASE_DIR, "igreja.db")
 
+
 def get_connection():
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
-    conn.execute("PRAGMA foreign_key = ON")
+    conn.execute("PRAGMA foreign_keys = ON")
     return conn
+
 
 def init_db():
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute("DROP TABLE IF EXISTS posts")
-    cursor.execute("DROP TABLE IF EXISTS users")
-    cursor.execute("DROP TABLE IF EXISTS newsletter")
-
     cursor.execute("""
-    CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    active INTEGER DEFAULT 1
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        active INTEGER DEFAULT 1
     )
-                   """)
+    """)
 
     cursor.execute("""
-    CREATE TABLE posts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    content TEXT,
-    user_id INTEGER,
-    active INTEGER DEFAULT 1
-    )   
-                    """)
-
-    cursor.execute("""
-    CREATE TABLE newsletter (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT UNIQUE NOT NULL,
-    active INTEGER DEFAULT 1
+    CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        content TEXT,
+        user_id INTEGER,
+        active INTEGER DEFAULT 1
     )
-                    """)
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS newsletter (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE NOT NULL,
+        active INTEGER DEFAULT 1
+    )
+    """)
 
     conn.commit()
     conn.close()
